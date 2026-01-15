2e9c3d9c17350830079c9531934e2b98a9a2d116
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-15 15:27:34 +0530

    Adding Header component

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-const childDiv1 = React.createElement(
-    "div",
-    {},
-    React.createElement("label", {}, "Name: "),
-    React.createElement("input", { type: "text" })
-);
-const childDiv2 = React.createElement(
-    "div",
-    {},
-    React.createElement("label", {}, "Email: "),
-    React.createElement("input", { type: "email" })
-);
-const buttonElement = React.createElement("button", {}, "Submit");
-const parentDiv = React.createElement("div", {}, [
-    childDiv1,
-    childDiv2,
-    buttonElement,
-]);
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(parentDiv);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/components/Header";
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    );
+};
